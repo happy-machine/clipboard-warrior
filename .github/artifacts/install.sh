534798a8931194b61c324cb10fb9ae3c5334203f
Dan Stein <dan.stein@generalbioinformatics.com>
2023-02-06 20:02:41 +0000

    v0.1.4

--- .github/artifacts/install.sh
+++ .github/artifacts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mv ./clip /usr/local/bin/clip
 mv ./clipboarddb.json /usr/local/bin/clipboarddb.json
-export PATH=$PATH:/usr/local/bin
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bash_profile
+echo 'alias clip="cd /usr/local/bin/ && ./clip"' >> ~/.bash_profile
+source ~/.bash_profile
